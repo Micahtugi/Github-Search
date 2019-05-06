c05b53f7b16202c803e6dc228d5dcdda85951abe
Micah <micahkimathi@gmail.com>
2019-05-06 11:13:42 +0300

    Saved bootstrap to the angular project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FreeService } from './free.service';
-import { ProfileSearchComponent } from './profile-search/profile-search.component';
+
 import {  FormsModule } from '@angular/forms';
 import { NotFoundComponent } from './not-found/not-found.component';
 
@@ -13,7 +13,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileSearchComponent,
+    
     NotFoundComponent
    
   ],
--- src/app/profile-search/profile-search.component.ts
+++ src/app/profile-search/profile-search.component.ts
@@ -1,15 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import {FreeService } from '../free.service';
 
 @Component({
-  selector: 'app-profile-search',
-  templateUrl: './profile-search.component.html',
-  styleUrls: ['./profile-search.component.css']
-})
-export class ProfileSearchComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+  
